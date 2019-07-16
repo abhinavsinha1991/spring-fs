0df71795d56569937c9349702ec4de8925ebef35
Abhinav <abhinav.sinha@in.fujitsu.com>
2019-07-16 18:24:08 +0530

    Added support for auto initialization of data on startup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,14 @@
+drop table if exists exchange_value;
+
+CREATE TABLE exchange_value (
+  id bigint(20) NOT NULL,
+  conversion_multiple decimal(19,2) DEFAULT NULL,
+  currency_from varchar(255) DEFAULT NULL,
+  port int(11) NOT NULL,
+  currency_to varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
 values(10001,'USD','INR',65,0);
 insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
