3f2456133207dba6958dd0b1e43577bbf134ff29
Abhinav Sinha <sinha.abhinav1991@gmail.com>
2019-08-16 15:47:08 +0530

    Set default timezone to IST.

--- src/main/java/com/in28minutes/springboot/microservice/example/forex/SpringBootMicroserviceForexServiceApplication.java
+++ src/main/java/com/in28minutes/springboot/microservice/example/forex/SpringBootMicroserviceForexServiceApplication.java
@@ -1,11 +1,20 @@
 package com.in28minutes.springboot.microservice.example.forex;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 /*@EnableDiscoveryClient*/
 public class SpringBootMicroserviceForexServiceApplication {
+
+	@PostConstruct
+	public void init()
+	{
+		// Setting Spring Boot TimeZone
+		TimeZone.setDefault( TimeZone.getTimeZone( "IST" ) );
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMicroserviceForexServiceApplication.class, args);
